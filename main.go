dfa5dfd09a764f5c02d33b62842297a6e4ae9773
satriaa14 <satria_agung14@yahoo.com>
2019-02-15 14:52:49 +0700

    commiting my google analytics

--- main.go
+++ main.go
@@ -12,19 +12,6 @@ func main() {
 	if port == "" {
 		port = "3031"
 	}
-
-	// router := gin.New()
-
-	// router.Use(gin.Logger())
-	// router.LoadHTMLGlob("src/index.html")
-	// router.Static("/static", "static")
-
-	// router.GET("/", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "index.html", nil)
-	// })
-
-	// router.Run(":" + port)
-
 	svr := ":" + port
 	fs := http.FileServer(http.Dir("./src"))
 	fmt.Printf("Server run at port : %v", port)
